222814581074a374d473b1aad8b9c625b668700d
happicat <80961808+meelkmeelk@users.noreply.github.com>
2024-05-06 12:23:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def index():
 def about():
     return render_template('about.html')
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+
+with app.app_context():
+    db.create_all()
